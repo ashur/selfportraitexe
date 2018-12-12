e658e84db6480a782654caeca2416ad8cdb745da
Ashur Cabrera <hello@ashur.cab>
2018-12-12 08:02:10 -0800

    Add branch checkout

--- git-config.sh
+++ git-config.sh
@@ -1,2 +1,3 @@
-git config user.email selfportraitexe@cabreramade.co
-git config user.name "@selfportraitexe"
+# git config user.email selfportraitexe@cabreramade.co
+# git config user.name "@selfportraitexe"
+git checkout selfportraitexe
